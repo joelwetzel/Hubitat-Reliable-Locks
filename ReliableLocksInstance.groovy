81728ce4a7f7197456892ed59fbe8822d557d771
Joel Wetzel <jwetzel1492@gmail.com>
2019-05-11 10:03:25 -0500

    Config page updates, and control over debug logging

--- ReliableLocksInstance.groovy
+++ ReliableLocksInstance.groovy
@@ -58,7 +58,9 @@ def autoRefreshOption = [
 
 
 preferences {
-	page(name: "mainPage", title: "<b>Wrapped Lock:</b>", install: true, uninstall: true) {
+	page(name: "mainPage", title: "", install: true, uninstall: true) {
+		section(getFormat("title", "Reliable Lock Instance")) {
+		}
 		section("") {
 			input wrappedLock
 			input refreshTime
@@ -71,13 +73,6 @@ preferences {
 				defaultValue: true
 			)
 		}
-	}
-}
-
-
-def log(msg) {
-	if (enableDebugLogging) {
-		log.debug(msg)	
 	}
 }
 
@@ -178,4 +173,21 @@ def wrappedLockHandler(evt) {
 		reliableLock.markAsUnlocked()
 	}
 }
+
+
+def getFormat(type, myText=""){
+	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	if(type == "title") return "<h2 style='color:#1A77C9;font-weight: bold'>${myText}</h2>"
+}
+
+
+def log(msg) {
+	if (enableDebugLogging) {
+		log.debug(msg)	
+	}
+}
+
+
+
 
